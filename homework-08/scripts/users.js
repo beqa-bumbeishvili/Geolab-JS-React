c1ff929c3e988903c5b06d31714363f5a57ec518
Ninojakhveladze <Ninojakhveladze@users.noreply.github.com>
2020-06-14 03:55:54 +0400

    user.js

--- homework-08/scripts/users.js
+++ homework-08/scripts/users.js
@@ -1,39 +1,3 @@
-class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    static checkField(username, password) {
-        return username === "" || password === "";
-    }
-
-    static checkUsername(users, username) {
-        return users.find(user => user.username === username);
-    }
-
-    static checkEmail(email) {
-        let pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-        let validEmail = pattern.exec(email) !== null;
-
-        return validEmail;
-    }
-
-    static checkUser(users, possibleUsername, possibleEmail, possiblePassword) {
-        return users.find(user => user.username === possibleUsername
-            && user.email === possibleEmail
-            && user.password === possiblePassword);
-    }
-    
-    static checkPasswordStrength = password => {
-        let pattern = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[^\w\s]).{6,}$/;
-        let validPassword = pattern.exec(password) !== null;
-
-        return validPassword;
-    }
-}
-
 let users;
 
 (function() {
